270c445a3dd3c4661084c60fd0808a159f545958
Alexander <alexander@MacBook.local>
2019-05-30 17:02:22 +0400

    users migration created

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -1,11 +1,37 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers\Auth;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Tymon\JWTAuth\JWTAuth;
 
 class RegisterController extends Controller
 {
-    //
+    protected $auth;
+
+    public function __construct(JWTAuth $auth)
+    {
+        $this->auth = $auth;
+    }
+
+    public function register(Request $request)
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'country'   => $request->country,
+            'city'      => $request->city,
+            'birthday'  => $request->birthday,
+            'password' => bcrypt($request->password),
+            'sex' => $request->sex
+        ]);
+
+        $token = $this->auth->attempt($request->only('email', 'password'));
+
+        return $token;
+    }
 }
