f7f4eff17d3a0baa9795d5abbd5aa76986413ac5
Alexander <alexander@MacBook.local>
2019-05-30 18:42:19 +0400

    factory users added

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,7 +1,6 @@
 <?php
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-use App\User;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
 
@@ -16,12 +15,18 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(User::class, function (Faker $faker) {
+$factory->define(\App\Models\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => bcrypt('secret'), // password
         'remember_token' => Str::random(10),
+        'country' => 'Russian Federation',
+        'city' => $faker->randomElement(['Astrakhan', 'Moscow']),
+        'birthday' => $faker->date(),
+        'tiny_about' => $faker->text(30),
+        'sex' => $faker->randomElement(['male', 'female']),
+        'car' => $faker->boolean,
+        'lookfor' => $faker->randomElement(['male', 'female']),
     ];
 });
