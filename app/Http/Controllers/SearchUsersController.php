f701208d57be17f41c7e58401325cea5c0cd8a46
Alexander <alexander@MacBook.local>
2019-05-30 18:46:37 +0400

    search users added

--- app/Http/Controllers/SearchUsersController.php
+++ app/Http/Controllers/SearchUsersController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\UserResource;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
 
 class SearchUsersController extends Controller
 {
-    //
+    public function index(Request $request, User $user): JsonResource
+    {
+        $payload = [];
+
+        foreach ($request->all() as $key => $value){
+
+            if($key == 'page') continue;
+
+            $payload[] = [$key, $value];
+        }
+
+        if(count($payload) === 0){
+            $payload[] = ['city', $request->user()->city];
+        }
+
+        $users = $user->where($payload)->paginate(21);
+
+        return UserResource::collection($users);
+    }
 }
